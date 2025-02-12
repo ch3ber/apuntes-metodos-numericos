bfe79f37c7f083a1f370626f5b50f2bd393b2e8b
Eber Alejo <eber4202@hotmail.com>
2025-02-12 11:00:06 -0600

    Remove Netlify integration from Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import netlify from '@astrojs/netlify';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
