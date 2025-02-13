3d59195f1831d74bdcc7578d791dbf346d61f0a0
Eber Alejo <73416354+ch3ber@users.noreply.github.com>
2025-02-13 03:22:31 +0000

    FIX: delete hard code URL

--- src/config/validateEnv.ts
+++ src/config/validateEnv.ts
@@ -2,8 +2,7 @@ import {
   getSecret
 } from 'astro:env/server'
 
-// const host: string = getSecret('HOST') ?? 'http://localhost:1234' // string | undefined
-const host = 'https://didactic-capybara-j9x55jgwrvwhpp4x-4322.app.github.dev'
+const host: string = getSecret('HOST') ?? 'http://localhost:1234' // string | undefined
 
 export const envConfig = {
   host
